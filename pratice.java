9ba6eff72729e2cf2994fceccc8c725fd99b2b7c
Divyashankar <130635981+shankar99655@users.noreply.github.com>
2023-07-06 17:41:06 +0530

    Create pratice.java

---
+++ pratice.java
@@ -0,0 +1,29 @@
+package pack2;
+
+import java.util.Scanner;
+
+public class FindFirstNonRepetingChar {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+			System.out.println("enter the value");
+			String value = sc.next();
+			boolean val;
+			for(int i = 0; i < value.length(); i++)
+			{
+				val = true;
+				for(int j = 0; j < value.length(); j++)
+				{
+					if(i != j &&value.charAt(i) == value.charAt(j))
+					{	
+						val = false;
+						break;
+					}
+				}
+				if(val)
+				{
+					System.out.println(value.charAt(i) + " is the non repeating charactor");
+					break;
+				}
+			}
+	}
+}
